ebd0fd3f3e2a956857b481a4fe25d86713f0af09
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-05-04 22:13:12 +0500

    updated ground

--- components/Planter.jsx
+++ components/Planter.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Bounds, useGLTF } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 import * as THREE from "three";
 import { useStateStore } from "@/stores/store";
 import { Base } from "./Base";
 
-const index = 0;
 export function Planter({ index, color, ...props }) {
-  const {setActive} = useStateStore();
+  const { setActive } = useStateStore();
   return (
-    <group position={[0, 1.3, 0]} {...props} onClick={ () => setActive(index)} >
+    <group position={[0, 1.3, 0]} {...props} onClick={() => setActive(index)}>
       {Array.from({ length: 5 }).map((_val, index) => {
         return (
           <SinglePlanter
@@ -16,36 +15,30 @@ export function Planter({ index, color, ...props }) {
             position={[0, index * 1.99 + 1, 0]}
             rotation={[0, index % 2 === 0 ? Math.PI / 4 : 0, 0]}
           />
-
-          // <mesh scale={2}>
-          //   <boxGeometry/>
-          //   <meshStandardMaterial color={color}/>
-          // </mesh>
-
+          //
+        //   <mesh scale={2}>
+        //     <boxGeometry />
+        //     <meshStandardMaterial color={color} />
+        //   </mesh>
         );
       })}
       <Base position={[-0.3, 0, 0.2]} />
     </group>
   );
 }
+
 export function SinglePlanter({ color, ...props }) {
   const { nodes, materials } = useGLTF("/planter.glb");
   return (
-    <group {...props} dispose={null} >
-      <mesh geometry={nodes.Bergenia_Rotblum_Set065.geometry} material={materials['Sunflower_sf1hcdvf2.003']} scale={14.407} />
-      <mesh geometry={nodes.Bergenia_Rotblum_Set067.geometry} material={materials['Sunflower_sf1hcdvf2.003']} scale={14.407} />
-      <mesh geometry={nodes.Bergenia_Rotblum_Set069.geometry} material={materials['Sunflower_sf1hcdvf2.003']} scale={14.407} />
-      <mesh geometry={nodes.Bergenia_Rotblum_Set070.geometry} material={materials['Sunflower_sf1hcdvf2.003']} scale={14.407} />
-      <mesh geometry={nodes.Circle032.geometry} material={materials['Material.002']} scale={14.407} />
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.Bergenia_Rotblum_Set065.geometry} material={materials["Sunflower_sf1hcdvf2.003"]} scale={14.407} />
+      <mesh geometry={nodes.Bergenia_Rotblum_Set067.geometry} material={materials["Sunflower_sf1hcdvf2.003"]} scale={14.407} />
+      <mesh geometry={nodes.Bergenia_Rotblum_Set069.geometry} material={materials["Sunflower_sf1hcdvf2.003"]} scale={14.407} />
+      <mesh geometry={nodes.Bergenia_Rotblum_Set070.geometry} material={materials["Sunflower_sf1hcdvf2.003"]} scale={14.407} />
+      <mesh geometry={nodes.Circle032.geometry} material={materials["Material.002"]} scale={14.407} />
       {/* <mesh geometry={nodes.Circle033.geometry} material={materials['Material.001']} scale={14.407} /> */}
-      <mesh geometry={nodes.Circle033.geometry} scale={14.407} >
-        <meshStandardMaterial
-          side={THREE.DoubleSide}
-          color={color}
-          roughness={0.7}
-          metalness={0.1}
-          attach={"material"}
-        />
+      <mesh geometry={nodes.Circle033.geometry} scale={14.407}>
+        <meshStandardMaterial side={THREE.DoubleSide} color={color} roughness={0.7} metalness={0.1} attach={"material"} />
       </mesh>
     </group>
   );
--- components/Scene.jsx
+++ components/Scene.jsx
@@ -1,18 +1,17 @@
 "use client";
 import { useStateStore } from "@/stores/store";
-import { Cloud, Clouds, DragControls, Environment, OrbitControls, RoundedBox, Sky, useTexture } from "@react-three/drei";
+import { Cloud, Clouds, DragControls, Environment, OrbitControls, RoundedBox, Sky, Stats, useTexture } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { Planter } from "./Planter";
-import { Fern } from "./models/Fern";
-import { Weed } from "./models/Weed";
-import { Shrub } from "./models/Shrub";
+import Ground from "./Ground";
 
 const Scene = () => {
-  const garden = useStateStore();
+  // const garden = useStateStore();
   return (
     <div className="flex-grow w-4/5 -z-0">
       <Canvas camera={{ position: [0, 10, 45], zoom: 2 }}>
+        <Stats/>
         <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 3}/>
 
         <Plants />
@@ -32,46 +31,48 @@ const Aisle = (props) => {
     </mesh>
   );
 };
-const Ground = () => {
-  const { width, height } = useStateStore();
-  const terrain = useStateStore(state => state.terrain);
-  const grass = useTexture({
-    map: `/textures/${terrain}/diff.jpg`,
-    displacementMap: `/textures/${terrain}/disp.jpg`,
-    roughnessMap: `/textures/${terrain}/rough.jpg`,
-    aoMap: `/textures/${terrain}/ao.jpg`,
-    normalMap: `/textures/${terrain}/nor.jpg`,
-  });
-  return (
-    <>
-      <mesh /* scale={[width, 1, height]} */ rotation={[-Math.PI /2, 0, 0]}>
-        <planeGeometry args={[width, height, 100, 100]} />
-        <meshStandardMaterial {...grass} />
-      </mesh>
-      {/* <group position={[-width / 2, 1, -height / 2]} scale={1}> */}
-      {/*   {Array.from({ length: 5 }).map((_val, index) => { */}
-      {/*     return ( */}
-      {/*       <> */}
-      {/*         <Fern */}
-      {/*           position={[Math.random() * width, 0, Math.random() * height]} */}
-      {/*           rotation={[0,  Math.random() * Math.PI * 2,0]} */}
-      {/*         /> */}
-      {/*         <Shrub */}
-      {/*           position={[Math.random() * width, 0, Math.random() * height]} */}
-      {/*           rotation={[0,  Math.random() * Math.PI * 2,0]} */}
-      {/*         /> */}
-      {/**/}
-      {/*         <Weed */}
-      {/*           position={[Math.random() * width, 0, Math.random() * height]} */}
-      {/*           rotation={[0,  Math.random() * Math.PI * 2,0]} */}
-      {/*         /> */}
-      {/*       </> */}
-      {/*     ); */}
-      {/*   })} */}
-      {/* </group> */}
-    </>
-  );
-};
+// const Ground = () => {
+//   const { width, height } = useStateStore();
+//   const terrain = useStateStore(state => state.terrain);
+//   // const grass = useTexture({
+//   //   map: `/textures/${terrain}/diff.jpg`,
+//   //   displacementMap: `/textures/${terrain}/disp.jpg`,
+//   //   roughnessMap: `/textures/${terrain}/rough.jpg`,
+//   //   aoMap: `/textures/${terrain}/ao.jpg`,
+//   //   normalMap: `/textures/${terrain}/nor.jpg`,
+//   // });
+//   return (
+//     <>
+//       <mesh /* scale={[width, 1, height]} */ rotation={[-Math.PI /2, 0, 0]}>
+//         {/* <planeGeometry args={[width, height, 100, 100]} /> */}
+//         <planeGeometry args={[width, height, 1, 1]}/>
+//         <meshBasicMaterial color={'green'}/>
+//         {/* <meshStandardMaterial {...grass} /> */}
+//       </mesh>
+//       {/* <group position={[-width / 2, 1, -height / 2]} scale={1}> */}
+//       {/*   {Array.from({ length: 5 }).map((_val, index) => { */}
+//       {/*     return ( */}
+//       {/*       <> */}
+//       {/*         <Fern */}
+//       {/*           position={[Math.random() * width, 0, Math.random() * height]} */}
+//       {/*           rotation={[0,  Math.random() * Math.PI * 2,0]} */}
+//       {/*         /> */}
+//       {/*         <Shrub */}
+//       {/*           position={[Math.random() * width, 0, Math.random() * height]} */}
+//       {/*           rotation={[0,  Math.random() * Math.PI * 2,0]} */}
+//       {/*         /> */}
+//       {/**/}
+//       {/*         <Weed */}
+//       {/*           position={[Math.random() * width, 0, Math.random() * height]} */}
+//       {/*           rotation={[0,  Math.random() * Math.PI * 2,0]} */}
+//       {/*         /> */}
+//       {/*       </> */}
+//       {/*     ); */}
+//       {/*   })} */}
+//       {/* </group> */}
+//     </>
+//   );
+// };
 const Plants = () => {
   const garden = useStateStore();
   return (
